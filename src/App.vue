89248c82379f742476f66f57b25b8dc7def80819
Rijvy <rrijvy@gmail.com>
2024-03-26 16:49:44 +0600

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,20 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <nav>
-      <section class="brand">
-        <p>
-          // rijvy
-          <span>web dev</span>
-        </p>
+  <header class="container">
+    <nav class="flex justify-between items-center">
+      <section class="flex flex-col justify-start items-end">
+        <p class="text-xl">// rijvy</p>
+        <p class="text-sm text-blue-700">web dev</p>
       </section>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/portfolio">Portfolio</RouterLink>
-      <RouterLink to="/services">Services</RouterLink>
-      <RouterLink to="/resume">Resume</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
+      <section class="nav-items">
+        <RouterLink class="px-7" to="/">_home</RouterLink>
+        <RouterLink class="px-7" to="/about">_about</RouterLink>
+        <RouterLink class="px-7" to="/portfolio">_portfolio</RouterLink>
+        <RouterLink class="px-7" to="/services">_services</RouterLink>
+        <RouterLink class="px-7" to="/resume">_resume</RouterLink>
+        <RouterLink class="pl-7" to="/contact">_contact</RouterLink>
+      </section>
       <section class="visit-blog"></section>
     </nav>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
@@ -25,20 +25,10 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-nav {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  .brand {
-    font-size: large;
-    span {
-      font-size: small;
-    }
-  }
-
-  a {
-    padding: 0px 20px;
-  }
+.nav-items > a {
+  transition: transform 0.2s;
+}
+.nav-items > a:hover {
+  transform: scale(1.2);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,96 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main></main>
+  <main class="container">
+    <div class="text-center pt-16">
+      <p class="text-2xl text-white">// HI, I'M RIJVY, A...</p>
+      <div class="text-7xl text-cyan-700">
+        <span>{" </span>
+        <span ref="typedElement"></span>
+        <span> "}</span>
+      </div>
+    </div>
+    <div class="pt-16">
+      <div class="text-center">
+        <p class="text-rose-200">{{ '{#}' }}</p>
+        <p class="text-rose-200 text-xl">{{ '<services>' }}</p>
+        <p class="pt-5">I offer a comprehensive range of services tailored to meet the unique needs of each project. Specializing in full-stack web development, I excel in crafting robust and scalable software solutions using cutting-edge technologies such as C#, JavaScript, Python, and various frameworks like React.js, Vue.js, nodeJs, .NET CORE and Django. Whether it's developing custom web applications, designing intuitive user interfaces, or optimizing database performance, I bring a wealth of experience and expertise to every project. Let's collaborate to bring your software ideas to life and drive your business forward.</p>
+        <table class="mx-auto mt-5 text-2xl cursor-none">
+          <tbody>
+            <tr class="text-left">
+              <td class="px-10 hover:bg-neutral-800"><p>Web Development</p></td>
+              <td class="px-10 hover:bg-neutral-800"><p>Scripting and Automation</p></td>
+            </tr>
+            <tr class="text-right">
+              <td class="px-10 hover:bg-neutral-800"><p>Mobile App Development</p></td>
+              <td class="px-10 hover:bg-neutral-800"><p>Desktop App Development</p></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="pt-16">
+      <div class="text-center">
+        <p class="text-rose-200">{{ '{*}' }}</p>
+        <p class="text-rose-200 text-xl">{{ '<contact me>' }}</p>
+        <p class="pt-5">Ready to start your next project or have questions about my services? Don't hesitate to reach out! </p>
+        <div class="w-6/12 mx-auto pb-10">
+          <form class="text-start" action="" method="post">
+            <div class="md:flex items-center mt-12">
+                <div class="w-full md:w-1/2 flex flex-col">
+                    <label class="font-semibold leading-none text-gray-300">Name</label>
+                    <input type="text" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
+                </div>
+                <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-semibold leading-none text-gray-300">Phone</label>
+                    <input type="email" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
+                </div>
+            </div>
+            <div class="md:flex items-center mt-8">
+                <div class="w-full flex flex-col">
+                    <label class="font-semibold leading-none text-gray-300">Subject</label>
+                    <input type="text" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
+                </div>
+                
+            </div>
+            <div>
+                <div class="w-full flex flex-col mt-8">
+                    <label class="font-semibold leading-none text-gray-300">Message</label>
+                    <textarea type="text" class="h-40 text-base leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
+                </div>
+            </div>
+            <div class="flex items-center justify-center w-full">
+                <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                    Send message
+                </button>
+            </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Typed from 'typed.js'
+
+const typedElement = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  if (typedElement.value) {
+    new Typed(typedElement.value, {
+      strings: ['front-end developer', 'full stack engineer', 'web designer'],
+      smartBackspace: true,
+      startDelay: 1500,
+      typeSpeed: 60,
+      backSpeed: 40,
+      backDelay: 700,
+      loop: true,
+      shuffle: false,
+      loopCount: Infinity,
+      showCursor: true,
+      cursorChar: '|',
+      autoInsertCss: true
+    })
+  }
+})
+</script>
